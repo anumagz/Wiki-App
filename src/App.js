92f8c5d322fb21a92ed5f5126ec4ccfbc4beb2d3
anumagz <anumagz1412@gmail.com>
2022-01-06 16:13:03 +0530

    wiki app

--- src/App.js
+++ src/App.js
@@ -6,17 +6,57 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://en.wikipedia.org/wiki/JavaScript"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         JavaScipt
         </a>
+        <a
+          className="App-link"
+          href="https://en.wikipedia.org/wiki/React_(JavaScript_library)"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          React
+        </a>
+        <a
+          className="App-link"
+          href="https://en.wikipedia.org/wiki/MySQL"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          MySQL
+        </a>
+        <a
+          className="App-link"
+          href="https://en.wikipedia.org/wiki/PHP"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          PHP
+        </a>
+        <a
+          className="App-link"
+          href="https://en.wikipedia.org/wiki/Java_(programming_language)"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Java
+        </a>
+        <a
+          className="App-link"
+          href="https://en.wikipedia.org/wiki/C_(programming_language)"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          C
+        </a>
+
+       
       </header>
     </div>
   );
